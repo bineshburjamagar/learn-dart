577a2459ef79faa3625f832ea72678065b46541a
Binesh Burja Magar <bineshburzaa@gmail.com>
2022-05-03 12:22:41 +0545

    if else condition

---
+++ conditions/ifelse.dart
@@ -0,0 +1,12 @@
+//if else Conditional Statement
+void main(List<String> args) {
+  int number = 100;
+
+  if (number % 2 == 0) {
+    print('even');
+  } else if (number % 3 == 0) {
+    print('odd');
+  } else {
+    print('confusedd');
+  }
+}
---
+++ conditions/switchCase.dart
@@ -0,0 +1,13 @@
+void main() {
+  int number = 0;
+  switch (number) {
+    case 0:
+      print('even');
+      break;
+    case 1:
+      print('odd');
+      break;
+    default:
+      print('confused');
+  }
+}
