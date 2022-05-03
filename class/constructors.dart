29f8a2bb0e5576de7729ef0b1425f8cf583d27f8
Binesh Burja Magar <bineshburzaa@gmail.com>
2022-05-03 14:14:07 +0545

    default and named constructorrs

---
+++ class/constructors.dart
@@ -0,0 +1,34 @@
+class Person {
+  var name;
+  var age;
+//constructor
+  /* Person(var name, var age) {
+    this.name = name;
+    this.age = age;
+  }
+*/
+  Person(this.name, [this.age = 12]); //default constructor
+
+  //named constructor
+  Person.guest() {
+    name = 'Guest';
+    age = 12;
+  }
+
+  void showDetails() {
+    print(name);
+    print(age);
+  }
+}
+
+void main(List<String> args) {
+  Person person1 = Person('bine', 2); //objects
+
+  person1.showDetails();
+
+  Person person2 = Person('gg');
+  person2.showDetails();
+
+  Person person3 = Person.guest();
+  person3.showDetails();
+}
