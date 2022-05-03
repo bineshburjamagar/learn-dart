27ecc673bc13334131b56fe16158af31f4ca0c78
Binesh Burja Magar <bineshburzaa@gmail.com>
2022-05-03 12:49:12 +0545

    do while loop

---
+++ loops/whileLoop.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  var num = 5;
+
+  // while (num > 0) {
+  //   print(num);
+  //   num -= 1;
+  // }
+  //do while
+
+  do {
+    print(num);
+    num -= 1;
+  } while (num > 0);
+}
